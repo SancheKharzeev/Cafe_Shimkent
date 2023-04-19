4f2b6a1123feab3b7a939e9e0b798fbeba55f7b2
Aleksandr Kh <akumasanta@gmail.com>
2023-04-19 14:37:33 +0600

    Initial Commit

---
+++ Cafe_Shimkent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cafe_Shimkent
+//
+//  Created by Александр Х on 19.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
